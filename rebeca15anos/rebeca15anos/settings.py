9552d2001ad657d55146ad94fc8412974dfa4233
Felipe Graf <bolinfel@gmail.com>
2026-04-20 20:28:23 -0300

    prod config

--- rebeca15anos/rebeca15anos/settings.py
+++ rebeca15anos/rebeca15anos/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ug54%o$xi*69a#w@)jz0@jop!)4m(4xoo!@o^dp&n*#ois@sq6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['137.184.128.223']
 
 
 # Application definition
